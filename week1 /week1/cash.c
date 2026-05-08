a12665578e56f07b5051af64a0ca5ebb9b5f9966
beyza6155 <byzsrc2161@gmail.com>
2026-05-08 05:40:47 +0300

    cash.c

---
+++ week1 /week1/cash.c
@@ -0,0 +1,68 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// Function prototypes
+int get_cents(void);
+int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
+
+int main(void)
+{
+    // Get the number of cents owed
+    int cents = get_cents();
+
+    // Calculate the number of each coin
+    int quarters = calculate_quarters(cents);
+    cents -= quarters * 25;
+
+    int dimes = calculate_dimes(cents);
+    cents -= dimes * 10;
+
+    int nickels = calculate_nickels(cents);
+    cents -= nickels * 5;
+
+    int pennies = calculate_pennies(cents);
+
+    // Print the total number of coins
+    printf("%i\n", quarters + dimes + nickels + pennies);
+}
+
+// Prompt the user for a non-negative number of cents
+int get_cents(void)
+{
+    int cents;
+    do
+    {
+        cents = get_int("Change owed: ");
+    }
+    while (cents < 0);
+    
+    return cents;
+}
+
+// Return how many quarters fit in `cents`
+int calculate_quarters(int cents)
+{
+    // Integer division in C automatically discards the remainder
+    return cents / 25;
+}
+
+// Return how many dimes fit in `cents`
+int calculate_dimes(int cents)
+{
+    return cents / 10;
+}
+
+// Return how many nickels fit in `cents`
+int calculate_nickels(int cents)
+{
+    return cents / 5;
+}
+
+// Return how many pennies fit in `cents`
+int calculate_pennies(int cents)
+{
+    return cents;
+}
